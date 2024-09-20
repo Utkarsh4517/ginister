864b97bc3e99c406af9a76f75126dcbe7a963fc8
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-09-20 05:32:42 +0530

    created rootcmd command

--- cmd/root.go
+++ cmd/root.go
@@ -13,23 +13,20 @@ import (
 
 
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ginister",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "CLI to generate Go-Gin projects with MySQL and GORM",
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter the project name: ")
+        projectName, _ := reader.ReadString('\n')
+        createProject(projectName)
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	},
+	
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
