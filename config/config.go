5c7b2f3a74de0af22daed74c159f2ec8ad9bbb26
Bharadwajshivam28 <shivambharadwaj822@gmail.com>
2024-09-20 06:26:01 +0530

    feat: strucutre

--- config/config.go
+++ config/config.go
@@ -1,21 +1,36 @@
 package config
 
 import (
-    "fmt"
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
+	"fmt"
+	"os"
+)
+
+func GenerateConfigFile(projectName string) {
+	configFile := fmt.Sprintf("%s/config/config.go", projectName)
+	file, _ := os.Create(configFile)
+	defer file.Close()
+
+	content := `package config
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectDB () {
+func ConnectDB() {
 	dsn := "user:password@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        fmt.Println("Failed to connect to database!")
-        return
-    }
+	if err != nil {
+		fmt.Println("Failed to connect to database!")
+		return
+	}
+	DB = database
+	fmt.Println("Database connection successful!")
+}`
 
-    DB = database
-    fmt.Println("Database connection successful!")
+	file.WriteString(content)
+	fmt.Println("Config file created successfully!")
 }
--- models/generateModel.go
+++ models/generateModel.go
@@ -1,18 +1,23 @@
+// models/generateModel.go
 package models
+
 import (
 	"fmt"
 	"os"
+	"strings"
 )
-func generateModelFile(modelName string, fields []string) {
-	modelFile := fmt.Sprintf("./models/%s.go", modelName)
-    file, _ := os.Create(modelFile)
-    defer file.Close()
 
-    file.WriteString(fmt.Sprintf("package models\n\nimport \"gorm.io/gorm\"\n\ntype %s struct {\n", modelName))
-    for _, field := range fields {
-        file.WriteString("\t" + field + "\n")
-    }
-    file.WriteString("\tgorm.Model\n}\n")
-    
-    fmt.Printf("Model for %s created successfully!\n", modelName)
+func GenerateModelFile(projectName, modelName string, fields []string) {
+	modelFile := fmt.Sprintf("%s/models/%s.go", projectName, strings.ToLower(modelName))
+	file, _ := os.Create(modelFile)
+	defer file.Close()
+
+	content := fmt.Sprintf("package models\n\nimport \"gorm.io/gorm\"\n\ntype %s struct {\n", modelName)
+	for _, field := range fields {
+		content += "\t" + field + "\n"
+	}
+	content += "\tgorm.Model\n}\n"
+
+	file.WriteString(content)
+	fmt.Printf("Model for %s created successfully!\n", modelName)
 }
